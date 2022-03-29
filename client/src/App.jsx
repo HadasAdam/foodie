04d2f74c5b3dc15ddc3d2e8f933d5855b18e1656
Amit Kastel <Amit.Kastel@brainlab.com>
2022-03-29 18:27:15 +0300

    added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import NavigationBar from './components/NavigationBar/NavigationBar'
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/Home/HomePage';
+import LoginPage from './pages/Login/LoginPage';
+import DashboardPage from './pages/Dashboard/DashboardPage';
 
 //This is the root component of our application
 const App = () => {
-  return (
-    <div>
-        <NavigationBar />
-    </div>
-  )
+    return (
+        <div>
+            <NavigationBar />
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<HomePage />} />
+                    <Route path='login' element={<LoginPage />} />
+                    <Route path='dashboard' element={<DashboardPage />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    )
 }
 
 export default App
