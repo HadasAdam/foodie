df57f7d38cd598ba4c7c14cec73a499a0666dc7f
Amit Kastel <Amit.Kastel@brainlab.com>
2022-03-29 18:00:31 +0300

    added reset.scss & global.scss

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+//global styles imports
+import "./assets/fonts/roboto/stylesheet.css";
+import "./assets/css/reset.scss";
+import "./assets/css/global.scss";
+
 ReactDOM.render(
   <React.StrictMode>
     <h1>Hello world</h1>
