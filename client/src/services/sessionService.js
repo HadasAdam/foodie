4ef53c1ec2817d997df9f1f0637a95a2c8798437
Amit Kastel <Amit.Kastel@brainlab.com>
2022-03-29 20:21:04 +0300

    added comments & callback parameter

--- client/src/services/sessionService.js
+++ client/src/services/sessionService.js
@@ -1,9 +1,19 @@
-export const login = async (username, password) => {
+/*
+    function to send login request to server, if the login request was successful, call the callback function 
+    with the token retrieved by the server
+*/
+export const login = async (username, password, callback) => {
     //made to act like a server, waits a second and then replies
     await new Promise(t => setTimeout(t, 1000));
 
     //simulates a login, future logic should be on the server
     if(username === "admin" && password === "password"){
+        //TODO: retrieve JWT from server
+        let token = '123';
+        if(callback !== null){
+            callback(token);
+        }
+
         return true;
     } else {
         return false;
