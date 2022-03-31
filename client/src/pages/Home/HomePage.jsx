48114159c96446ed557b6c7ce2d48103854b8bdb
Amit <amitkastel3@gmail.com>
2022-03-31 18:25:27 +0300

    made it so the homepage displays posts

--- client/src/pages/Home/HomePage.jsx
+++ client/src/pages/Home/HomePage.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./HomePage.scss";
+import Post from './Post/Post';
+import { getAllPosts } from "../../services/postsService";
 const HomePage = () => {
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [posts, setPosts] = useState([]); 
+
+  //useEffect to run when the page loads
+  useEffect(() => {
+    
+    //useEffect is synchronous, so we need to create a seperate asynchronous function for data fetching
+    const fetchData = async () => {
+
+      //getting the data from the server
+      setIsLoading(true);
+      const posts = await getAllPosts();
+      setIsLoading(false);
+
+      //sets the posts list state
+      setPosts(posts);
+
+    }
+
+    //gets all of the posts from the server and puts them in "posts" state
+    fetchData();
+
+  }, []);
+
   return (
-    <div>HomePage</div>
+    <main className="homepage">
+      {/* Shows loading indication while loading posts from server */}
+      {isLoading && <h2>Loading posts...</h2>}
+
+      {/* Goes through all of the posts, and renders them */}
+      {
+        posts.map(post => 
+          <Post
+            title={post.title}
+            text={post.text}
+            imageLink={post.imageLink}
+            videoLink={post.videoLink}
+            key={post.title}
+          />)
+      }
+    </main>
   )
 }
 
