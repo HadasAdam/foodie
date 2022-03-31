fb66cb77d8d0bf41ae096d8a3634c303e2c0a521
Amit <amitkastel3@gmail.com>
2022-03-31 20:03:37 +0300

    added management menu to be as aside

---
+++ client/src/components/ManagementMenu/ManagementMenu.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./ManagementMenu.scss";
+
+const ManagementMenu = () => {
+    return (
+        <aside className='management-menu'>
+            ManagementMenu
+        </aside>
+    )
+}
+
+export default ManagementMenu
