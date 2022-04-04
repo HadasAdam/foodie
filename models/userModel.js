02e15197b9669416cdec0875ba98806ae098a95e
Amit Kastel <Amit.Kastel@brainlab.com>
2022-04-04 10:27:19 +0300

    added user model

---
+++ models/userModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    gender: {
+        type: String, 
+        required: true
+    }, 
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+
+module.exports = User;
