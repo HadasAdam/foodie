49abe1204373cfe36190866733a0406ec1e3ec24
Amit Kastel <Amit.Kastel@brainlab.com>
2022-04-04 14:46:44 +0300

    integrated login request

--- client/src/services/sessionService.js
+++ client/src/services/sessionService.js
@@ -3,17 +3,32 @@
     with the token retrieved by the server
 */
 export const login = async (username, password) => {
-    //made to act like a server, waits a second and then replies
-    await new Promise(t => setTimeout(t, 1000));
 
-    //simulates a login, future logic should be on the server
-    if(username === "admin" && password === "password"){
-        //TODO: retrieve JWT from server
-        window.isLoggedIn = true;
-        return true;
-    } else {
-        return false;
-    }
+    //turns the object to json to send to server
+    const body = JSON.stringify({username, password});
+
+    //indicator for login success/fail
+    let success = false;
+
+    //sending the login request to the server
+    await fetch("/api/users/login", {
+        method: "POST",
+        body: body,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    .then(res => res.json())
+    .then(res => {
+        //if login is successful, removes it from 
+        if(res.success)
+        {
+            localStorage.setItem('jwt', res.token);
+            console.log(res.token);
+            success = true;
+        }
+    });
+    return success;
 }
 
 export const signIn = async (username, firstName, lastName, gender, password) => {
@@ -28,5 +43,5 @@ export const isLoggedIn = () => {
 }
 
 export const logout = () => {
-    window.isLoggedIn = false;
+    localStorage.removeItem('jwt');
 }
