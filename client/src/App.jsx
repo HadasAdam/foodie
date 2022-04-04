c34ef5106537e41d5d736ab245172c90300c73a2
Amit Kastel <Amit.Kastel@brainlab.com>
2022-04-04 14:46:33 +0300

    added context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavigationBar from './components/NavigationBar/NavigationBar'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/Home/HomePage';
@@ -7,19 +7,27 @@ import Footer from './components/Footer/Footer';
 import NewAuthorPage from './pages/Author/NewAuthorPage';
 import ManagementMenu from './components/ManagementMenu/ManagementMenu';
 
+//application context
+export const AppContext = React.createContext();
+
 //This is the root component of our application
 const App = () => {
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
     return (
-        <BrowserRouter>
-            <NavigationBar />
-            <ManagementMenu />
-            <Routes>
-                <Route path='/' element={<HomePage />} />
-                <Route path='/NewAuthor' element = {<NewAuthorPage/>}/>
-                <Route path='login' element={<LoginPage />} />
-            </Routes>
-            <Footer />
-        </BrowserRouter>
+        <AppContext.Provider value={{isLoggedIn, setIsLoggedIn}}>
+            <BrowserRouter>
+                <NavigationBar />
+                {isLoggedIn && <ManagementMenu />}
+                <Routes>
+                    <Route path='/' element={<HomePage />} />
+                    <Route path='/NewAuthor' element={<NewAuthorPage />} />
+                    <Route path='login' element={<LoginPage />} />
+                </Routes>
+                <Footer />
+            </BrowserRouter>
+        </AppContext.Provider>
     )
 }
 
