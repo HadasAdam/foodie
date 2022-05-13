3f93cdb60590f457f8ea17111e4b1551fe9914db
david mijan <david.mijan@zeekit.me>
2022-05-13 16:50:26 +0300

    change back to full connection string

--- config.js
+++ config.js
@@ -6,10 +6,8 @@ const MONGO_PORT = process.env.MONGO_PORT || "27017";
 const DB_ADDRESS = process.env.DB_ADDRESS || `localhost:${MONGO_PORT}`;
 const PORT = process.env.PORT || 5000;
 module.exports = {
-    mongoConnectionString: `mongodb://${DB_ADDRESS}/${DB_NAME}`, // database connection string
+    mongoConnectionString: `mongodb://${DB_USERNAME}:${DB_PASSWORD}@${DB_ADDRESS}/${DB_NAME}`, // database connection string
     dbName: DB_NAME, // database name
     jwtSecret: JWTSECRET, // secret for encrypting the jwt token
     port: PORT //port to run the application in
 }
-
-//+ DB_USERNAME + ':' + DB_PASSWORD + '@' +
