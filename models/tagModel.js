2ecaefb3722ec832e3ca70543611b2f9925f156e
david mijan <david.mijan@zeekit.me>
2022-05-30 02:28:17 +0300

    Add update, getbyid, getall | Refactor

--- models/tagModel.js
+++ models/tagModel.js
@@ -1,17 +1,18 @@
 const mongoose = require('mongoose');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 const tagSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
     },
-    id: {
-        type: Number,
-        required: true
+    _id: {
+        type: Number
     }
 });
 
-// const sequenceCollection = new mongoose.Collection("sequence")
 
+tagSchema.plugin(AutoIncrement, {id:'order_seq',inc_field: '_id'});
 const Tag = mongoose.model('Tag', tagSchema);
 module.exports = Tag;
+
